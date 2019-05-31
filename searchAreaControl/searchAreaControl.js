40520cb3102d6dcd48180cb71b061b6b034b2880
kapantzak <i.kapantzakis@epsilonnet.gr>
2019-05-31 16:42:42 +0300

    Added uuid popup id

--- searchAreaControl/searchAreaControl.js
+++ searchAreaControl/searchAreaControl.js
@@ -93,9 +93,16 @@
          * Get unique popup id
          */
         _getNewPopupID: function (pluginName) {
-            var thisPopupID = pluginName + '_Popup_' + ($('body').find('.' + this.rootClassName).length + 1);
+            var thisPopupID = pluginName + '_Popup_' + (this._uuidv4());
             this.popupID = thisPopupID;
             return this.popupID;
+        },
+
+        _uuidv4: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
         },
 
         _toggleBodyScrolling: function(scroll) {
